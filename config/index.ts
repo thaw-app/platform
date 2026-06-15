9b0143c78661e909a6c33c69b769f030b210f23e
rene <diaz.de.sandi.7@gmail.com>
2026-06-14 20:28:43 -0700

    feat: add members export to config index

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,5 @@
 export { default as labels } from "./labels.yaml";
+export { default as members } from "./members.yaml";
 export { default as org } from "./org.yaml";
 export { default as repos } from "./repos.yaml";
 export { default as rulesets } from "./rulesets.yaml";
